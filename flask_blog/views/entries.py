94e2b472bd0ee086cb4c28475bc27ac4a726c99f
Kento.Yamada <masterhinagiku@gmail.com>
2021-08-24 21:02:16 +0900

    models 作成後のviews entries.py

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,31 +1,25 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from flask_blog.models.entries import Entry
 from datetime import datetime
 
 
 @app.route('/')
 def show_entries():
-    entries = [
-        {
-            'id': 1,
-            'title': 'はじめての投稿',
-            'text': 'はじめての内容',
-            'created_at': datetime.now(),
-        },
-        {
-            'id': 2,
-            'title': '２つめの投稿',
-            'text': '２つめの内容',
-            'created_at': datetime.now(),
-        },
-    ]
+    entries = Entry.scan()
+    entries = sorted(entries, key=lambda x: x.id, reverse=True)
     return render_template('entries/index.html', entries=entries)
 
 
 @app.route('/entries', methods=['POST'])
 def add_entry():
-    # 記事の作成処理を実装
-    return '新しく記事が作成されました'
+    entry = Entry(
+        id=int(datetime.now().timestamp()),
+        title=request.form['title'],
+        text=request.form['text']
+    )
+    entry.save()
+    return redirect(url_for('show_entries'))
 
 
 @app.route('/entries/new', methods=['GET'])
@@ -35,46 +29,27 @@ def new_entry():
 
 @app.route('/entries/<int:id>', methods=['GET'])
 def show_entry(id):
-    entry = {
-        'id': 1,
-        'title': 'はじめての投稿',
-        'text': 'はじめての内容',
-        'created_at': datetime.now(),
-
-    }
+    entry = Entry.get(id)
     return render_template('entries/show.html', entry=entry)
 
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
 def edit_entry(id):
-    entries = [
-        {
-            'id': 1,
-            'title': 'はじめての投稿',
-            'text': 'はじめての内容',
-            'created_at': datetime.now(),
-        },
-        {
-            'id': 2,
-            'title': '２つめの投稿',
-            'text': '２つめの内容',
-            'created_at': datetime.now(),
-        },
-    ]
-    entry = None
-    for e in entries:
-        if e['id'] == id:
-            entry = e
+    entry = Entry.get(id)
     return render_template('entries/edit.html', entry=entry)
 
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
 def update_entry(id):
-    # 記事の更新処理を実装
-    return f'記事{id}が更新されました'
+    entry = Entry.get(id)
+    entry.title = request.form['title']
+    entry.text = request.form['text']
+    entry.save()
+    return redirect(url_for('show_entries'))
 
 
 @app.route('/entries/<int:id>/delete', methods=['POST'])
 def delete_entry(id):
-    # 記事の削除処理を実装
-    return f'記事{id}が削除されました'
+    entry = Entry.get(id)
+    entry.delete()
+    return redirect(url_for('show_entries'))
